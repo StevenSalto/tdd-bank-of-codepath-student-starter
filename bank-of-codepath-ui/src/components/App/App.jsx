2aeaa3ccfb4b0b172b53c2a1cf1b9df4cdf4b54d
SSalto <73043956+StevenSalto@users.noreply.github.com>
2022-06-23 02:00:26 -0400

    Update

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -1,13 +1,29 @@
 import * as React from "react"
 import Navbar from "../Navbar/Navbar"
 import Home from "../Home/Home"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import TransactionDetail from "../TransactionDetail/TransactionDetail"
 import "./App.css"
 
 export default function App() {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [transactions, setTransactions] = React.useState([]);
+  const [transfers, setTransfers] = React.useState([]);
+  const [error, setError] = React.useState("");
+  const [filterInputValue, setFilterInputValue] = React.useState("");
+
+
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
+    <div className="app">
+      <BrowserRouter>
+        <Navbar filterInputValue={filterInputValue} setFilterInputValue={setFilterInputValue}/>
+        <main>
+          <Routes>
+            <Route to="/" element={<Home transactions={transactions} setTransactions={setTransactions} transfers={transfers} setTransfers={setTransfers} error={error} setError={setError} isLoading={isLoading} setIsLoading={setIsLoading} filterInputValue={filterInputValue}/>}/>
+            <Route to="/transactions/:transactionId" element={<TransactionDetail />}/>
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   )
 }
